b0ce44773cf48f036dc308f95a9df585258ba916
Tanghong Qiu <taqiu@silo.soic.indiana.edu>
2013-10-09 20:17:59 -0400

    update semaphore

--- apps/ex4.c
+++ apps/ex4.c
@@ -3,11 +3,11 @@
  * is just a part of shell. Shell run this program as a
  * function
  */
-/*#include <stdio.h>
- *#include <stdlib.h>
- */
+#include <stdio.h>
+#include <stdlib.h>
+ 
 
-void produce(void), consume(void);
+void produce(int consumed, int produced), consume(int consumed, int produced);
 int n = 0; /* external variables are shared by all processes */
 
 /*------------------------------------------------------------------------
@@ -16,18 +16,25 @@ int n = 0; /* external variables are shared by all processes */
  */
 void run_ex4(void) {
     n = 0;
-    resume(create(consume, 1024, 20, "cons", 0));
-    resume(create(produce, 1024, 20, "prod", 0));
+    int produced, consumed;
+    consumed = semcreate(0);
+    produced = semcreate(1);
+
+    resume(create(consume, 1024, 20, "cons", consumed, produced));
+    resume(create(produce, 1024, 20, "prod", consumed, produced));
 }
 
 /*------------------------------------------------------------------------
  * produce -- increment n 2000 times and exit
  *------------------------------------------------------------------------
  */
-void produce(void) {
+void produce(int consumed, int produced) {
     int i;
-    for( i=1 ; i<=2000 ; i++ )
+    for( i=1 ; i<=2000 ; i++ ) {
+        wait(consumed);
         n++;
+        signal(produced);
+    }
 }
 
 /*------------------------------------------------------------------------
@@ -35,9 +42,12 @@ void produce(void) {
  *------------------------------------------------------------------------
  */
 
-void consume(void) {
+void consume(int consumed, int produced) {
     int i;
-    for( i=1 ; i<=60 ; i++ )
-        printf("The value of n is %d\n", n);
+    for( i=1 ; i<=2000 ; i++ ) {
+        wait(produced);
+        printf("n is %d\n", n);
+        signal(consumed);
+    }
 }
 
