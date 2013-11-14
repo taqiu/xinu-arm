974422bca4c631a7b443483983f5d3e65ef1f864
TIANHAO CAO <caotia@cyclops.soic.indiana.edu>
2013-11-14 07:01:41 -0500

    future2

--- apps/ex4.c
+++ apps/ex4.c
@@ -1,33 +1,71 @@
 #include <future.h>
 
-void future_cons(future);
-void future_prod(future);
+void future_cons1(future);
+void future_cons2(future);
+void future_prod1(future);
+void future_prod2(future);
+void fun(future);
+void fun_cont(future);
 
 void run_ex4(void) {
-    future f1, f2, f3;
-    f1 = future_alloc(0);
-    f2 = future_alloc(0);
-    f3 = future_alloc(0);
+    future f1 , f2, f3, f4;
+    int ret = 10;
 
-    resume( create(future_cons, 1024, 20, "fcons1", 1, f1) );
-    resume( create(future_prod, 1024, 20, "fprod1", 1, f1) );
-    resume( create(future_cons, 1024, 20, "fcons2", 1, f2) );
-    resume( create(future_prod, 1024, 20, "fprod2", 1, f2) );
-    resume( create(future_cons, 1024, 20, "fcons3", 1, f3) );
-    resume( create(future_prod, 1024, 20, "fprod3", 1, f3) );
+    // Test for FT_SHARED
+    f1 = future_alloc(FT_SHARED);
+
+    resume( create(future_cons1, 1024, 20, "fcons1", 1, f1) );
+    resume( create(future_cons1, 1024, 20, "fcons2", 1, f1) );
+    resume( create(future_cons1, 1024, 20, "fcons3", 1, f1) );
+    resume( create(future_prod1, 1024, 20, "fprod1", 1, f1) );
+    
+    // Test for FT_QUEUE
+    f2 = future_alloc(FT_QUEUE);
+    resume( create(future_cons2, 1024, 20, "fcons1", 1, f2) );
+    resume( create(future_prod1, 1024, 20, "fprod1", 1, f2) );
+    resume( create(future_prod2, 1024, 20, "fprod2", 1, f2) );
+    
+    // Test for asynch()
+    f3 = future_alloc(FT_NULL);    // FT_NULL will make the behavior same as the future implemeted with binary semaphore
+    asynch(&f3, fun);
+
+    // Test for cont()
+    f4 = future_alloc(FT_SHARED);
+    resume( create(cont, 1024, 20, "cont", 2, &f4, fun_cont));
+    //cont(&f4, fun_cont);
+    future_set(&f4, &ret);
 }
 
-void future_cons(future fut) {
+void fun_cont(future fut) {
+    printf("future has been set\n\r");
+}
+
+void fun(future fut) {
+    int ret = 10;
+    future_set(&fut, &ret);
+    printf("asynch(future*, void *) will run and set the future to %d\n\r", ret);
+}
+
+void future_cons1(future fut) {
     int ret;
-    int i;
-    for (i = 0; i < 3; i++) {
-    	future_get(fut, &ret);
-    	printf("future %d value is %d\n\r", fut, ret);
+    future_get(fut, &ret);
+    printf("future %d value is %d\n\r", fut, ret);
+}
+
+void future_cons2(future fut) {
+    int ret, i;
+    for (i = 0; i < 2; i ++) {
+        future_get(fut, &ret);
+        printf("future %d value is %d\n\r", fut, ret);
     }
 }
 
-void future_prod(future fut) {
-    int i;
-    for (i = 0; i < 3; i++)
-        future_set(fut, i);
+void future_prod1(future fut) {
+    int ret = 1;
+    future_set(&fut, &ret);
+}
+
+void future_prod2(future fut) {
+    int ret = 2;
+    future_set(&fut, &ret);
 }
--- include/future.h
+++ include/future.h
@@ -3,10 +3,20 @@
 
 #include <stddef.h>
 #include <thread.h>
+#include <queue.h>
+#include <thread.h>
 
-#define NFUT 40
 #define FUT_USED 1
 #define FUT_FREE 0
+#define FT_NULL 0
+#define FT_SHARED 1
+#define FT_QUEUE 2
+
+#define FUT_INIED 1
+#define FUT_UNINI 0
+
+#define NFUT 40
+#define VAL_QUE_LEN 100
 
 #define isbadfut(f) ((int)(f) < 0 || (f) >= NFUT)
 
@@ -15,6 +25,7 @@ int future_initialized = 0;
 typedef unsigned int future;
 struct futent {
    int future_flag;
+   int flag;
    int fstate;
    int value;
    int sem_prod, sem_cons;
@@ -22,17 +33,23 @@ struct futent {
 
 struct futent futent_tab[NFUT];
 
+int value_que[VAL_QUE_LEN];
+int head, tail;
+
 future future_alloc(int future_flags);
 syscall future_free(future);
 syscall future_get(future, int *);
-syscall future_set(future, int);
+syscall future_set(future *, int *);
 void future_ini();
 future future_new();
 
 void future_ini() {
     int i;
+    tail = -1;
+    head = 0;
     for (i = 0; i < NFUT; i ++) {
         futent_tab[i].fstate = FUT_FREE;
+        futent_tab[i].future_flag = FT_NULL;
     }
 }
 
@@ -50,6 +67,7 @@ future future_new() {
         if (nextfut >= NFUT) nextfut = 0;
         if (futent_tab[fut].fstate == FUT_FREE) {
                 futent_tab[fut].fstate = FUT_USED;
+                futent_tab[fut].flag = FUT_UNINI;
                 return fut;
         }
     }
@@ -61,46 +79,114 @@ future future_alloc(int future_flags) {
     irqmask mask;
     mask = disable();
     future fut;
+
     if ((fut = future_new()) == SYSERR) {
         restore(mask);
         return SYSERR;
     }
 
-    if ((futent_tab[fut].sem_prod = semcreate(0)) == SYSERR || (futent_tab[fut].sem_cons = semcreate(1)) == SYSERR) {
-        restore(mask);
-        return SYSERR;
+    struct futent *futptr = &futent_tab[fut];
+    futptr->future_flag = future_flags;
+
+    if (future_flags == FT_SHARED) {
+        if ((futptr->sem_prod = semcreate(0)) == SYSERR) {
+            restore(mask);
+            return SYSERR;
+        }
+    } else if (future_flags == FT_QUEUE) {
+        if ((futptr->sem_prod = semcreate(0)) == SYSERR) {
+            restore(mask);
+            return SYSERR;
+        }
+    } else {
+	// future without flag
+	//printf("Debug[alloc]\n\r");
+        if ((futent_tab[fut].sem_prod = semcreate(0)) == SYSERR || (futent_tab[fut].sem_cons = semcreate(1)) == SYSERR) {
+            restore(mask);
+            return SYSERR;
+        }
     }
 
     restore(mask);
     return fut;
-    
 }
 
 syscall future_get(future fut, int *value) {
     irqmask mask;
+    register struct thrent *thrptr;
+    register struct sement *semptr;
+
     mask = disable();
     if (isbadfut(fut)) {
         restore(mask);
         return SYSERR;
     }
-    wait(futent_tab[fut].sem_prod);
-    *value = futent_tab[fut].value;
-    signal(futent_tab[fut].sem_cons);
 
+    struct futent* futptr = &futent_tab[fut];
+
+    if (futptr->future_flag == FT_SHARED) {
+        if (futptr->flag == FUT_INIED) {
+            *value = futent_tab[fut].value;
+        } else if (futptr->flag == FUT_UNINI) {
+            thrptr = &thrtab[thrcurrent];
+            semptr = &semtab[futptr->sem_prod];
+            thrptr->state = THRWAIT;
+            thrptr->sem = futptr->sem_prod;
+            enqueue(thrcurrent, semptr->queue);
+            resched();
+            *value = futent_tab[fut].value;
+        }
+    } else if (futptr->future_flag == FT_QUEUE) {
+         wait(futptr->sem_prod);
+         if (tail >= head) {
+             *value = value_que[head];
+             head ++;
+         } else wait(futptr->sem_prod);
+    } else {
+        printf("Debug[get]\n\r");
+        // future without flag
+        wait(futent_tab[fut].sem_prod);
+        *value = futent_tab[fut].value;
+        signal(futent_tab[fut].sem_cons);
+    }
     restore(mask);
     return OK;
 }
 
-syscall future_set(future fut, int value) {
+syscall future_set(future *future, int *value) {
+    register struct thrent *thrptr;
+    register struct sement *semptr;
+    int fut = *future;
     irqmask mask;
     mask = disable();
     if (isbadfut(fut)) {
         restore(mask);
         return SYSERR;
     }
-    wait(futent_tab[fut].sem_cons);
-    futent_tab[fut].value = value;
-    signal(futent_tab[fut].sem_prod);
+
+    struct futent *futptr = &futent_tab[fut];
+
+    if (futptr->future_flag == FT_SHARED) {
+        if (futptr->flag == FUT_UNINI) {
+            semptr = &semtab[futptr->sem_prod];
+            futptr->value = *value;
+            futptr->flag = FUT_INIED;
+            while(nonempty(semptr->queue)) {
+                ready(dequeue(semptr->queue), RESCHED_YES);
+            }
+        } else if (futptr->flag == FUT_INIED) {
+            futptr->value = *value;
+        }
+    } else if (futptr->future_flag == FT_QUEUE) {
+        tail ++;
+        value_que[tail] = *value;
+        signal(futptr->sem_prod);
+    } else {
+        //printf("Debug[set]\n\r");
+        wait(futent_tab[fut].sem_cons);
+        futent_tab[fut].value = *value;
+        signal(futent_tab[fut].sem_prod);
+    }
 
     restore(mask);
     return OK;
@@ -114,12 +200,46 @@ syscall future_free(future fut) {
         return SYSERR;
     }
 
-    if (semfree(futent_tab[fut].sem_prod) == SYSERR || semfree(futent_tab[fut].sem_cons) == SYSERR) {
+    struct futent *futptr = &futent_tab[fut];
+
+    if (futptr->future_flag == FT_SHARED) {
+    } else if (futptr->future_flag == FT_QUEUE) {
+    } else {
+        if (semfree(futent_tab[fut].sem_prod) == SYSERR || semfree(futent_tab[fut].sem_cons) == SYSERR) {
+            restore(mask);
+            return SYSERR;
+        }
+    }
+    futptr->fstate = FUT_FREE;
+    futptr->flag = FUT_UNINI;
+
+    restore(mask);
+    return OK;
+}
+
+
+syscall asynch(future *fut, void *fun) {
+    irqmask mask;
+    mask = disable();
+    if (isbadfut(*fut)) {
         restore(mask);
         return SYSERR;
     }
-    futent_tab[fut].fstate = FUT_FREE;
+    resume( create(fun, 1024, 20, "future_asynch", 1, *fut));
+    restore(mask);
+    return OK;
+}
 
+syscall cont(future *fut, void *fun) {
+    irqmask mask;
+    mask = disable();
+    if (isbadfut(*fut)) {
+        restore(mask);
+        return SYSERR;
+    }
+    int ret;
+    if (future_get(*fut, &ret) == OK)
+    resume( create(fun, 1024, 20, "future_cont", 1, *fut));
     restore(mask);
     return OK;
 }
